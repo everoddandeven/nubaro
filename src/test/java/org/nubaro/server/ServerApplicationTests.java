d5c778d6c3d4826c93867ca8f71bf93981b2a2e1
everoddandeven <everoddandeven@protonmail.com>
2025-06-20 18:03:37 -0400

    Add base server application

---
+++ src/test/java/org/nubaro/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nubaro.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
